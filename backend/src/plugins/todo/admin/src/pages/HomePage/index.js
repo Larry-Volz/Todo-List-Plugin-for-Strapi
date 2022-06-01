e311bec8c49221cae92e428a6f28a29f934d8579
Larry Volz <imaginologist@gmail.com>
2022-06-01 02:32:22 -0400

    front end working up to 1 todo

--- backend/src/plugins/todo/admin/src/pages/HomePage/index.js
+++ backend/src/plugins/todo/admin/src/pages/HomePage/index.js
@@ -15,15 +15,45 @@ import { Illo } from "../../components/Illo";
 import { Button } from "@strapi/design-system/Button"
 import Plus from "@strapi/icons/Plus"
 import TodoModal from "../../components/TodoModal";
+import TodoCount from "../../components/TodoCount";
+
+import TodoTable from "../../components/TodoTable";
 
 
 const HomePage = () => {
-  const [ todoData, setTodoData ] = useState([]);
+
+  //testing > 0
+//   const [ todoData, setTodoData ] = useState([
+//     {
+//     id: 1,
+//     name: "test"
+//     },
+//     {
+//       id: 2,
+//       name: "2nd test"
+//       },
+// ]);
+
+const [ todoData, setTodoData ] = useState([]);
+
+
   const [showModal, setShowModal] = useState(false);
 
   async function addTodo(data) {
     setTodoData([...todoData, { ...data, id: nanoid(), isDone: false }]);
 
+  }
+
+  async function toggleTodo(data) {
+    alert("Add Toggle Todo in API");
+  }
+
+  async function deleteTodo(data) {
+    alert("Add Delete Todo in API");
+  }
+
+  async function editTodo(id, data) {
+    alert("Add Edit Todo in API");
   }
 
   return (
@@ -32,11 +62,12 @@ const HomePage = () => {
 
       <BaseHeaderLayout
         title="Larry's To Do Plugin"
-        subtitle="Keeping it real and organized since 2022"
+        subtitle="TCB & Get 'er done"
         as="h2"
       />
       
       <ContentLayout>
+      
       {todoData.length === 0 ? (
           <EmptyStateLayout
             icon={<Illo />}
@@ -52,7 +83,21 @@ const HomePage = () => {
             }
           />
         ) : (
-          <h2>You have {todoData.length} count</h2>
+          <>
+          {/* <h2>You have {todoData.length} count</h2> */}
+          <TodoCount count = {todoData.length} />
+
+          {
+          
+           <TodoTable
+              todoData={todoData}
+              setShowModal={setShowModal}
+              toggleTodo={toggleTodo}
+              deleteTodo={deleteTodo}
+              editTodo={editTodo}
+            /> }
+
+          </>
         )}
 
       </ContentLayout>
